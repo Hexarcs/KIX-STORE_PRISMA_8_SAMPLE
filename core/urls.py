22f049e2ea6ceae19ad655c7422858929d55c672
Hexarcs <hexarcsgloba@gmail.com>
2026-08-24 16:29:03 -0300

    Add Docker support and update application configuration and readme update

--- core/urls.py
+++ core/urls.py
@@ -29,9 +29,5 @@ urlpatterns = [
 ]
 
 if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-    
-    
-    # Adiciona o mapeamento para servir arquivos estáticos e de mídia
-urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
